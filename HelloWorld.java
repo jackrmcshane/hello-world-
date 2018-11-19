b2cff0fa7676f0b895f405303613fda75e96ec5e
jackrmcshane <jackrmcshane@gmail.com>
2018-11-18 20:11:07 -0600

    Nothing of note.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main( String[] args ){
 		System.out.println( "Hello World!" );
 		System.out.println( "I am very glad I am getting this to work!" );
+		System.out.println( "Github is starting to look pretty sweeet!" );
 	}
 }
